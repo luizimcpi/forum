65211400eee33b0bb2c033a55a98a5b72a7b65ce
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-08-31 23:22:08 -0300

    criando cadastro de topico

--- src/main/kotlin/br/com/devlhse/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/devlhse/forum/controller/TopicoController.kt
@@ -4,6 +4,8 @@ import br.com.devlhse.forum.model.Topico
 import br.com.devlhse.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,5 +21,10 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico{
         return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico){
+        service.cadastrar(topico)
     }
 }
