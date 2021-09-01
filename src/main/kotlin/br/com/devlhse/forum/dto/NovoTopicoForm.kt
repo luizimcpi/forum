53435991d565371922a21864881e880da33e0878
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-09-01 00:30:19 -0300

    add validations

--- src/main/kotlin/br/com/devlhse/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/devlhse/forum/dto/NovoTopicoForm.kt
@@ -1,10 +1,14 @@
 package br.com.devlhse.forum.dto
 
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
 data class NovoTopicoForm (
-        val titulo: String,
-        val mensagem: String,
-        val idCurso: Long,
-        val idAutor: Long
+        @field:NotEmpty @Size(min = 5, max = 100)val titulo: String,
+        @field:NotEmpty val mensagem: String,
+        @field:NotNull val idCurso: Long,
+        @field:NotNull val idAutor: Long
 )
 
 
