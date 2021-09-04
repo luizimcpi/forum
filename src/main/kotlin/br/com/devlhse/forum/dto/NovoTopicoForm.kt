73a1f542c4b8aeaf317c6976f53a118b746608cb
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-09-04 00:27:45 -0300

    Adicionando tratamento para erros de validação de entrada

--- src/main/kotlin/br/com/devlhse/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/devlhse/forum/dto/NovoTopicoForm.kt
@@ -5,10 +5,17 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
-        @field:NotEmpty @Size(min = 5, max = 100)val titulo: String,
-        @field:NotEmpty val mensagem: String,
-        @field:NotNull val idCurso: Long,
-        @field:NotNull val idAutor: Long
+        @field:NotEmpty @Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres")
+        val titulo: String,
+
+        @field:NotEmpty
+        val mensagem: String,
+
+        @field:NotNull
+        val idCurso: Long,
+
+        @field:NotNull
+        val idAutor: Long
 )
 
 
--- src/main/kotlin/br/com/devlhse/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/devlhse/forum/exception/ExceptionHandler.kt
@@ -1,7 +1,10 @@
 package br.com.devlhse.forum.exception
 
 import br.com.devlhse.forum.dto.ErrorView
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -20,6 +23,39 @@ class ExceptionHandler {
             status = NOT_FOUND.value(),
             error = NOT_FOUND.name,
             message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    fun handleServerError(
+        exception: Exception,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = INTERNAL_SERVER_ERROR.value(),
+            error = INTERNAL_SERVER_ERROR.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach {
+            e ->
+            errorMessage[e.field] = e.defaultMessage
+        }
+        return ErrorView(
+            status = BAD_REQUEST.value(),
+            error = BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
